6c49947624c10777c16c71a7c06c9a169025934c
sharfealam900 <sharfealam900@gmail.com>
2026-08-22 18:31:13 +0530

    aug21

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://soukfashionhouse.onrender.com/api/v1",
-   baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://soukfashionhouse.onrender.com/api/v1",
+  //  baseURL: "http://localhost:8000/api/v1",
   withCredentials: true,
 });
 export default api;
